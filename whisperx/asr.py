05a155437ca9e1e6320bd5f3caa104fa4503f240
denisbetsi <denis@intosales.com>
2025-02-01 19:07:46 -0400

    language detection skipped when language provided
    language detection skipped when language provided

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -293,8 +293,8 @@ class FasterWhisperPipeline(Pipeline):
         }
 
     def detect_language(self, audio: np.ndarray) -> tuple[str, float]:
-        if audio.shape[0] < N_SAMPLES:
-            print("Warning: audio is shorter than 30s, language detection may be inaccurate.")
+        # if audio.shape[0] < N_SAMPLES:
+            # print("Warning: audio is shorter than 30s, language detection may be inaccurate.")
         model_n_mels = self.model.feat_kwargs.get("feature_size")
         segment = log_mel_spectrogram(audio[: N_SAMPLES],
                                       n_mels=model_n_mels if model_n_mels is not None else 80,
@@ -303,7 +303,7 @@ class FasterWhisperPipeline(Pipeline):
         results = self.model.model.detect_language(encoder_output)
         language_token, language_probability = results[0][0]
         language = language_token[2:-2]
-        print(f"Detected language: {language} ({language_probability:.2f}) in first 30s of audio...")
+        # print(f"Detected language: {language} ({language_probability:.2f}) in first 30s of audio...")
         return language, language_probability
 
 
